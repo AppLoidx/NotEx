0e5b60e53d19f3d99c5946e4f9ba437528dfd151
apploid <apploidyakutsk@gmail.com>
2021-10-27 12:45:44 +0300

    add notex flow example

---
+++ src/test/java/NotExFlow.java
@@ -0,0 +1,44 @@
+import com.apploidxxx.notex.Notification;
+import com.apploidxxx.notex.Result;
+import org.junit.Test;
+
+public class NotExFlow {
+    @Test
+    public void flow() {
+        Integer number = 0;
+
+        String result = Result.<Integer, String>of(number)
+                .apply(this::minusOne)
+                .apply(this::minusTwo)
+                .apply(this::minusThree)
+                .resolve(num -> "Number is " + num, errorObject -> errorObject);
+        System.out.println(result);
+
+
+    }
+
+
+    private Result<Integer, String> minusOne(Integer number) {
+        if (number < 1) {
+            return Result.of(Notification.of("Negative error (1)"));
+        } else {
+            return Result.of(number - 1);
+        }
+    }
+
+    private Result<Integer, String> minusTwo(Integer number) {
+        if (number < 2) {
+            return Result.of(Notification.of("Negative error (2)"));
+        } else {
+            return Result.of(number - 2);
+        }
+    }
+
+    private Result<Integer, String> minusThree(Integer number) {
+        if (number < 3) {
+            return Result.of(Notification.of("Negative error (3)"));
+        } else {
+            return Result.of(number - 3);
+        }
+    }
+}
