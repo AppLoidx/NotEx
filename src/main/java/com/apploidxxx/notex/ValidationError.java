847343a064cade015bdda0d5e10c6b3bf54ae38b
apploid <apploidyakutsk@gmail.com>
2022-03-22 23:58:44 +0300

    use concurrent hash map

--- src/main/java/com/apploidxxx/notex/ValidationError.java
+++ src/main/java/com/apploidxxx/notex/ValidationError.java
@@ -1,11 +1,11 @@
 package com.apploidxxx.notex;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.BiConsumer;
 
 public class ValidationError<T, S> {
-    private final Map<T, S> errorMap = new HashMap<>();
+    private final Map<T, S> errorMap = new ConcurrentHashMap<>();
     private boolean hasError;
 
     public void addError(T field, S value) {
